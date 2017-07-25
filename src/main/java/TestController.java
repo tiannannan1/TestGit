c2651a09d018034e1dba50d4289fbba142fb1d65
tnn <672210580@qq.com>
2017-07-25 17:03:48 +0800

    modify TestController java file

--- src/main/java/TestController.java
+++ src/main/java/TestController.java
@@ -2,4 +2,11 @@
  * Created by khcm on 2017/7/25.
  */
 public class TestController {
+
+    public String test(String param) {
+
+        System.out.println("TestGit");
+
+        return "";
+    }
 }
